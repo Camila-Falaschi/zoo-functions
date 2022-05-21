552cba0597f57404f5b901161774a5ffd85a753d
Camila <falaschi.camila@yahoo.com.br>
2022-05-21 00:33:12 -0300

    Requisito 1: Implementando a função getSpeciesByIds - concluido :tada: :star_struck: :tada:

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,14 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+// Para o argumento '...ids' e return 'ids' foi recebido orientação do Márcio Daniel, na Mentoria Técnica da Trybe.
+
+function getSpeciesByIds(...ids) {
+  // param === parameter
+  return ids.map((param) => species.find((animal) => animal.id === param));
 }
+
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce',
+  'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5'));
 
 module.exports = getSpeciesByIds;
