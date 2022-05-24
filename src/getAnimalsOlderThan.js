bb1a43a96e239e72faf25e8475b67c34c2bf7323
Camila <falaschi.camila@yahoo.com.br>
2022-05-24 16:58:08 -0300

    Requisito 2: implemente função getAnimalsOlderThan - concluido

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,11 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const findingAnimal = species.find((info) => info.name === animal).residents
+    .map((resident) => resident.age);
+  return findingAnimal.every((animalAge) => animalAge >= age);
 }
 
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -8,7 +8,6 @@ function getSpeciesByIds(...ids) {
   return ids.map((param) => species.find((animal) => animal.id === param));
 }
 
-console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce',
-  'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5'));
+console.log(getSpeciesByIds());
 
 module.exports = getSpeciesByIds;
