c7da958b66104a17ae57a9a5c1820918fff4d56a
Camila <falaschi.camila@yahoo.com.br>
2022-05-29 02:51:06 -0300

    Requisitos 10: Implemente a função getOldestFromFirstSpecies - completo

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -2,6 +2,11 @@ const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
+  // Para 'reduce' na linha 8 foi consultado o Stack OverFlow (https://stackoverflow.com/questions/4020796/finding-the-max-value-of-an-attribute-in-an-array-of-objects)
+  const employee = () => data.employees.find((person) => person.id === id).responsibleFor[0];
+  const findOldestAnimal = data.species.find((animal) => animal.id === employee())
+    .residents.reduce((acc, animal) => ((acc.age < animal.age) ? animal : acc));
+  return Object.values(findOldestAnimal);
 }
 
 module.exports = getOldestFromFirstSpecies;
