fe0ca8063d47008907c4f3826a5db62034ef520d
Camila <falaschi.camila@yahoo.com.br>
2022-05-29 16:25:43 -0300

    Requisitos 11: Implemente a função getEmployeesCoverage - completo

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,36 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
+const locations = (animals) => animals.map((element) =>
+  data.species.find((info) => info.name === element).location);
+
+const species = (animalsId) => animalsId.map((element) =>
+  data.species.find((info) => info.id === element).name);
+
+const object = (employee) => {
+  const { id, firstName, lastName, responsibleFor } = employee;
+  return {
+    id,
+    fullName: `${firstName} ${lastName}`,
+    species: species(responsibleFor),
+    locations: locations(species(responsibleFor)),
+  };
+};
+
+const allEmployees = () => data.employees.map((info) => object(info));
+
+function getEmployeesCoverage(param) {
   // seu código aqui
+  if (param === undefined) {
+    return allEmployees();
+  }
+  const { name, id } = param;
+  const employeesData = data.employees;
+  const employee = () => employeesData.find((info) =>
+    info.firstName === name || info.lastName === name || info.id === id);
+  if (employee() === undefined) {
+    throw new Error('Informações inválidas');
+  }
+  return object(employee());
 }
 
 module.exports = getEmployeesCoverage;
